7939c938874c72c1f4789f534020df3deab066b5
Raquel VIBE <ana.santos@vibetecnologia.com>
2023-01-31 14:32:49 -0300

    validacoes no formulario -menos data- mapeamento de variaveis

--- desafio_vibe/lib/paginas/ted_formulario.dart
+++ desafio_vibe/lib/paginas/ted_formulario.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../modelos/ted.dart';
-
 class TedFormulario extends StatelessWidget {
   const TedFormulario({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(      
       appBar: AppBar(
         title: const Text('Dados da TED',style: TextStyle(color: Colors.white)),
       ),
@@ -42,29 +40,49 @@ class _FormularioState extends State<Formulario> {
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Código do Banco'),
                 validator: (value) {
-                  if(value == null || value.trim().isEmpty) {return 'Código Inválido';}
+                  if(value == null || value.trim().isEmpty) {return 'Código do Banco é obrigatório';}
+                  if(value.trim().length < 3) {return 'Código do banco deve possuir até 3 dígitos';}
                 },
-                onSaved: (value) => _formTed['codBanco'] = value as Object,
+                onSaved: (value) => _formTed['codBanco'] = value!,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Agência'),
-                onSaved: (value) => _formTed['agencia'] = value as Object,                
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) {return 'Agência é obrigatória';}
+                  if(value.trim().length < 4) {return 'Agência deve possuir até 4 dígitos';}
+                },
+                onSaved: (value) => _formTed['agencia'] = value!,                
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Conta'),
-                onSaved: (value) => _formTed['conta'] = value as Object,
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) {return 'Conta é obrigatória';}
+                  if(value.trim().length < 10) {return 'Conta deve possuir até 10 dígitos';}
+                },
+                onSaved: (value) => _formTed['conta'] = value!,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'CPF'),
-                onSaved: (value) => _formTed['cpf'] = value as Object,
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) {return 'CPF é obrigatório';}
+                  if(value.trim().length < 11) {return 'CPF deve possuir 11 dígitos';}
+                },
+                onSaved: (value) => _formTed['cpf'] = value!,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Valor (R\$)'),
-                onSaved: (value) => _formTed['valor'] = value as Object,
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) {return 'Valor é obrigatório';}
+                  if(double.parse(value) > 0 && double.parse(value) <= 10000) {return 'Valor deve ser um valor numérico superior a R\$ 0,00 e inferior a R\$ 10.000,00';}
+                },
+                onSaved: (value) => _formTed['valor'] = value!,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Data'),
-                onSaved: (value) => _formTed['data'] = value as Object,),                
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) {return 'Data é obrigatória';}
+                },
+                onSaved: (value) => _formTed['data'] = value!,),                
               Container(
                 padding: const EdgeInsets.all(20),
                 child: Row(
@@ -84,6 +102,7 @@ class _FormularioState extends State<Formulario> {
 
                           if (isValid == true) {
                             _form.currentState?.save();
+                            print(_formTed);
                             Navigator.of(context).pop();
                           }                          
                         },
